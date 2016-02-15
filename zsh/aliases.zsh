46395b9aad6ce07e2eaaf8d632be757cc63fcba1
Justin A. Baker <bakermoto@gmail.com>
2016-02-14 23:07:55 -0500

    hmm bleh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,13 +3,3 @@ alias bx="bundle exec"
 alias :q="exit"
 alias :t="tail -n1000 -f $@"
 alias :p="ps aux | grep -v grep | grep $@ -i --color=auto"
-alias :spec="bundle exec rspec $@"
-
-# default filetype actions
-alias -s js="vim"
-alias -s coffee="vim"
-alias -s rb="vim"
-alias -s haml="vim"
-alias -s pdf="open"
-
-# alias tmux='TERM=screen-256color-bce tmux'
